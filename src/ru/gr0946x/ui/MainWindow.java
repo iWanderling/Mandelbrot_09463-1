8dcf04d3cf8212d69a9c5f2ea0c9763ee91f60e1
On1G1ri <on1g1r119283745@gmail.com>
2026-04-23 17:03:40 +0300

    Added open and save functions in MainWindow

--- src/ru/gr0946x/ui/MainWindow.java
+++ src/ru/gr0946x/ui/MainWindow.java
@@ -5,11 +5,20 @@ import ru.gr0946x.ui.fractals.Fractal;
 import ru.gr0946x.ui.fractals.Mandelbrot;
 import ru.gr0946x.ui.painting.FractalPainter;
 import ru.gr0946x.ui.painting.Painter;
-
+import javax.swing.JFileChooser;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import javax.swing.*;
 import java.awt.*;
 
 import static java.lang.Math.*;
+import static jdk.jfr.consumer.EventStream.openFile;
 
 public class MainWindow extends JFrame {
 
@@ -18,6 +27,14 @@ public class MainWindow extends JFrame {
     private final Fractal mandelbrot;
     private final Converter conv;
     public MainWindow(){
+        // Создание меню
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Файл");
+        JMenuItem openItem = new JMenuItem("Открыть");
+        openItem.addActionListener(e -> openFile());
+        fileMenu.add(openItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(800, 650));
         mandelbrot = new Mandelbrot();
@@ -56,5 +73,33 @@ public class MainWindow extends JFrame {
                 .addComponent(mainPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE)
                 .addGap(8)
         );
+    }
+    private void openFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Открыть фрактал");
+        fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Фракталы (*.frac)", "frac"));
+
+        int result = fileChooser.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                // Читаем сохраненные параметры
+                double savedXMin = ois.readDouble();
+                double savedXMax = ois.readDouble();
+                double savedYMin = ois.readDouble();
+                double savedYMax = ois.readDouble();
+
+                // Восстанавливаем границы конвертера
+                conv.setXShape(savedXMin, savedXMax);
+                conv.setYShape(savedYMin, savedYMax);
+
+                // Перерисовываем
+                mainPanel.repaint();
+
+                JOptionPane.showMessageDialog(this, "Файл успешно открыт", "Успех", JOptionPane.INFORMATION_MESSAGE);
+            } catch (IOException | ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(this, "Ошибка при открытии файла: " + e.getMessage(), "Ошибка", JOptionPane.ERROR_MESSAGE);
+            }
+        }
     }
 }
